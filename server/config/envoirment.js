df9384cedd589b2ba3517559e653f1c7b561107b
Andon Mitev <mitevandon94@gmail.com>
2019-07-07 01:29:39 -0700

    Implement signin user functionality and JSON web token generated too

--- server/config/envoirment.js
+++ server/config/envoirment.js
@@ -1,6 +1,7 @@
 module.exports = {
     dev: {
         port: 7000,
-        connectionString: 'mongodb://localhost:27017/decentralized-store'
+        connectionString: 'mongodb://localhost:27017/decentralized-store',
+        jwtKey: 'secret'
     }
 }
--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,10 +1,13 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const User = require('mongoose').model('User');
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('mongoose').model('User');
+const env = require('../config/envoirment');
+
+
 
 const onSignUp = async (req, res) => {
-    const { email, password } = req.body;
+    const { body: { email, password } } = req;
 
     try {
         const isExistingUser = await User.findOne({ email });
@@ -17,23 +20,59 @@ const onSignUp = async (req, res) => {
         }
 
         const hashedPassword = await bcrypt.hash(password, 10);
-
         const user = await User.create({ email, password: hashedPassword });
 
         return res.status(201)
-                .json({
-                    message: 'User successfully created ' + user.email,
-                    email: user.email
-                });
+            .json({
+                message: 'User successfully created ' + user.email,
+                email: user.email
+            });
     } catch (error) {
-        console.error(error);
+        return res.status(500)
+            .json({ error });
     }
 
 
 }
 
-const onSignIn = (req, res) => {
+const onSignIn = async (req, res) => {
+    const { body: { email, password } } = req;
 
+    try {
+        const user = await User.findOne({ email });
+
+        // If user do not exists
+        if (!user) {
+            return res.status(401)
+                .json({
+                    message: 'Authentication failed!'
+                });
+        }
+
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+        if (isPasswordMatch) {
+            const { email, _id } = user;
+
+            const token = await jwt.sign({email, _id}, env.dev.jwtKey, {expiresIn: '2 days'});
+
+            return res.status(200)
+                .json({
+                    message: 'Authentication successful!',
+                    token
+                });
+        }
+
+        // If user exists but password is incorrect
+        return res.status(401)
+            .json({
+                message: 'Authentication failed!'
+            });
+
+    } catch (error) {
+        return res.status(500)
+            .json({ error });
+    }
 }
 
 module.exports = router
