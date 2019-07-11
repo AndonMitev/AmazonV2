3632fa0ac21cce6f113369ac278ab08c088e4773
Andon Mitev <mitevandon94@gmail.com>
2019-07-10 21:49:08 -0700

    Add message section

--- client/src/components/Details.vue
+++ client/src/components/Details.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <v-divider></v-divider>
     <v-layout row mt-2 justify-center>
       <v-flex xs10 mb-5>
         <v-carousel height="350" interval="3000">
@@ -79,6 +78,40 @@
         <GChart type="ColumnChart" :data="chartData" :options="chartOptions" />
       </v-flex>
     </v-layout>
+    <v-divider></v-divider>
+    <v-layout mt-5 row wrap>
+      <v-flex>
+        <h3>Comments</h3>
+        <v-list two-line>
+          <template v-for="(item, index) in people">
+            <v-subheader v-if="item.header" :key="item.header">{{ item.header }}</v-subheader>
+
+            <v-divider v-else-if="item.divider" :key="index" :inset="item.inset"></v-divider>
+
+            <v-list-tile v-else :key="item.title" avatar @click>
+              <v-list-tile-avatar>
+                <img :src="item.avatar" />
+              </v-list-tile-avatar>
+
+              <v-list-tile-content>
+                <v-list-tile-title v-html="item.title"></v-list-tile-title>
+                <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </template>
+        </v-list>
+      </v-flex>
+    </v-layout>
+    <v-layout mt-5 row wrap justify-end>
+      <v-flex xs12>
+        <v-textarea name="input-7-1" label="Message..."></v-textarea>
+      </v-flex>
+      <v-flex xs12>
+        <div >
+          <v-btn offset-md10 color="primary">Send</v-btn>
+        </div>
+      </v-flex>
+    </v-layout>
   </div>
 </template>
 
@@ -119,7 +152,30 @@ export default {
           title: "Company Performance",
           subtitle: "Sales, Expenses, and Profit: 2014-2017"
         }
-      }
+      },
+      people: [
+        { header: "Today" },
+        {
+          avatar: "https://cdn.vuetifyjs.com/images/lists/1.jpg",
+          title: "Brunch this weekend?",
+          subtitle:
+            "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?"
+        },
+        { divider: true, inset: true },
+        {
+          avatar: "https://cdn.vuetifyjs.com/images/lists/2.jpg",
+          title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
+          subtitle:
+            "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend."
+        },
+        { divider: true, inset: true },
+        {
+          avatar: "https://cdn.vuetifyjs.com/images/lists/3.jpg",
+          title: "Oui oui",
+          subtitle:
+            "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
+        }
+      ]
     };
   },
   components: {
