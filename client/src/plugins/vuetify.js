9d65874c625a56a4aba97c7befe487ad9f7aa93c
Andon Mitev <mitevandon94@gmail.com>
2019-07-13 10:50:05 -0700

    Override vetur rules for double quotes

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
 import 'vuetify/src/stylus/app.styl'
 import VueGoogleCharts from 'vue-google-charts'
- 
+
 Vue.use(VueGoogleCharts)
 Vue.use(Vuetify, {
   iconfont: 'md'
--- client/src/services/register-rules.js
+++ client/src/services/register-rules.js
@@ -1,40 +1,40 @@
 export default {
     firstNameRules() {
-        return v => !!v || "First name is required";
+        return v => !!v || 'First name is required';
     },
     lastNameRules() {
-        return v => !!v || "Last name is required";
+        return v => !!v || 'Last name is required';
     },
     emailRules() {
         return [
-            v => !!v || "E-mail is required",
-            v => /.+@.+/.test(v) || "E-mail must be valid"
+            v => !!v || 'E-mail is required',
+            v => /.+@.+/.test(v) || 'E-mail must be valid'
         ];
     },
     compareEmails() {
-        return this.userData.confirmEmail !== this.userData.email ? "Emails do not match" : true;
+        return this.userData.confirmEmail !== this.userData.email ? 'Emails do not match' : true;
     },
     passwordRules() {
         return [
-            v => !!v || "Password is required",
-            v => v.length >= 5 || "Password should be at least 5 symbols long",
-            v => v.length <= 10 || "Password cannot be more than 10 symbols long"
+            v => !!v || 'Password is required',
+            v => v.length >= 5 || 'Password should be at least 5 symbols long',
+            v => v.length <= 10 || 'Password cannot be more than 10 symbols long'
         ];
     },
     comparePasswords() {
         return this.userData.confirmPassword !== this.userData.password
-            ? "Passwords do not match"
+            ? 'Passwords do not match'
             : true;
     },
     addressRules() {
         return [
-            v => !!v || "Address is required",
-            v => v.length >= 3 || "Address should be at least 3 symbols long"
+            v => !!v || 'Address is required',
+            v => v.length >= 3 || 'Address should be at least 3 symbols long'
         ];
     },
     phoneRules() {
         return v =>
             /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im.test(v) ||
-            "Please enter valid phone number";
+            'Please enter valid phone number';
     }
 }
